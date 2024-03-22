12438e55f555396e61f12909f034e5f17de10450
Михаил Свинцицкий <starenginegames@gmail.com>
2024-03-23 01:07:31 +1000

    Init

---
+++ UDP-Client/UDP-Client.cpp
@@ -0,0 +1,75 @@
+﻿
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    WSAData wsaData;
+    WORD DLLVersion = MAKEWORD(2, 1);
+    if (WSAStartup(DLLVersion, &wsaData) != 0)
+        std::cout << "Ошибка инициализации WSA\n";
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(49001);
+    addr.sin_family = AF_INET;
+
+    SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+    if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr)) != 0) {
+        std::cout << "Ошибка подключения к серверу\n";
+        return 1;
+    }
+
+    char response[256];
+    recv(Connection, response, sizeof(response), NULL);
+    std::cout << response << std::endl;
+    
+    while (true)
+    {
+        std::cout << "\nВыберете команду: \n";
+        std::cout << "1) Узнать IP для доменного имени \n";
+        std::cout << "2) Выход \n";
+        std::cout << "Ваш выбор: ";
+
+        int clientInput;
+        std::cin >> clientInput;
+
+        switch (clientInput)
+        {
+            case 1:
+            {
+                char request[256];
+                std::cout << "\nВведите доменное имя: ";
+                std::cin >> request;
+
+                send(Connection, request, sizeof(request), NULL);
+
+                memset(response, 0, 255);
+                recv(Connection, response, 255, NULL);
+                std::cout << "Полученный IP от сервера: " << response << std::endl;
+
+                system("pause");
+                system("CLS");
+                break;
+            }
+            case 2: 
+            {
+                shutdown(Connection, 0);
+                closesocket(Connection);
+                return 0;
+            }   
+            default: 
+            {
+                std::cout << "\nКоманда не распознана! ";
+                break;
+            }
+        }
+    }
+
+}
---
+++ UDP-Server/UDP-Server.cpp
@@ -0,0 +1,77 @@
+﻿
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+char* GetIP(char domen[256])
+{
+    auto remoteHost = gethostbyname(domen);
+
+    char* ip = remoteHost->h_addr_list[0];
+
+    return inet_ntoa(**(in_addr**)remoteHost->h_addr_list);
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    WSAData wsaData;
+    if (WSAStartup(MAKEWORD(2, 1), &wsaData) != 0)
+        std::cout << "Ошибка инициализации WSA\n";
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(49001);
+    addr.sin_family = AF_INET;
+    int addrlenght = sizeof(addr);
+
+    SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+    bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+    listen(sListen, SOMAXCONN);
+
+    while (true)
+    {
+        std::cout << "Жду присоединения клиента\n";
+
+        SOCKET clientConnection = accept(sListen, (SOCKADDR*)&addr, &addrlenght);
+
+        if (clientConnection != 0)
+        {
+            std::cout << "Клиент подключен!\n";
+
+            char message[256] = "Вы успешно подключились!";
+            send(clientConnection, message, sizeof(message), NULL);
+
+            //Ждем и обрабатываем запрос от клиента
+            while (true)
+            {
+                char request[256];
+                recv(clientConnection, request, sizeof(request), NULL);
+
+                if (strlen(request) == 0) //Вот это не получилось
+                {
+                    std::cout << "Клиент отключился!" << request << std::endl;
+                    closesocket(clientConnection);
+                    break;
+                }
+
+                std::cout << "Поступил запрос: " << request << std::endl;
+
+                char* response;
+                response = GetIP(request);
+
+                std::cout << "Отправляю ответ: " << response << std::endl;
+                send(clientConnection, response, 255, NULL);
+
+                std::cout << std::endl;
+            }
+        }
+    }
+}
+
+
